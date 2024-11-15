830d64edb595d68fb971e53a0f61b60c73d1ff2b
urpagin <urpagin@domain.com>
2024-11-16 00:56:12 +0100

    Make the SLP working

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,9 +1,9 @@
 //! This module manages the TCP server and how/where the packets are managed/sent.
-
-use crate::config;
-use crate::packet::data_types::{varint, CodecError};
-use crate::packet::{Packet, PacketId};
-use log::{debug, warn};
+use crate::packet::data_types::{string, varint, CodecError};
+use crate::packet::Packet;
+use crate::{config, gracefully_exit};
+use byteorder::{BigEndian, ReadBytesExt};
+use log::{debug, error, info, warn};
 use std::net::SocketAddr;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
@@ -44,13 +44,13 @@ impl Default for ConnectionState {
 }
 
 /// Object representing a TCP connection.
-struct Connection {
+struct Connection<'a> {
     state: ConnectionState,
-    socket: TcpStream,
+    socket: &'a mut TcpStream,
 }
 
-impl Connection {
-    fn new(socket: TcpStream) -> Self {
+impl<'a> Connection<'a> {
+    fn new(socket: &'a mut TcpStream) -> Self {
         Self {
             state: ConnectionState::default(),
             socket,
@@ -66,25 +66,29 @@ async fn handle_connection(
     debug!("New connection: {addr}");
     // TODO: Maybe have a bigger/dynamic buffer?
     let mut buf = [0; BUFFER_SIZE];
-    let mut state = ConnectionState::default();
+    //let mut state = ConnectionState::default();
+    let mut connection = Connection {
+        state: ConnectionState::default(),
+        socket: &mut socket,
+    };
 
     loop {
-        let read_bytes = socket.read(&mut buf).await?;
+        let read_bytes = connection.socket.read(&mut buf).await?;
         if read_bytes == 0 {
             debug!("Connection closed: {addr}");
             return Ok(()); // TODO: Why Ok? It's supposed to be an error right?
         }
 
-        let response = handle_packet(&mut state, &buf[..read_bytes]).await?;
+        let response = handle_packet(&mut connection, &buf[..read_bytes]).await?;
 
         // TODO: Assure that sent packets are big endians (data types).
-        socket.write_all(&response).await?;
+        connection.socket.write_all(&response).await?;
     }
 }
 
 /// Takes a packet buffer and returns a reponse.
-async fn handle_packet(
-    state: &mut ConnectionState,
+async fn handle_packet<'a>(
+    conn: &'a mut Connection<'_>,
     buffer: &[u8],
 ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     print!("\n\n\n"); // So that each logged packet is clearly visible.
@@ -99,11 +103,35 @@ async fn handle_packet(
     debug!("PACKET ID: {packet_id_value}");
 
     match packet_id_value {
-        0x00 => match state {
+        0x00 => match conn.state {
             ConnectionState::Handshake => {
                 warn!("Handshake packet detected!");
-                let next_state = read_handshake_next_state(&packet).await;
-                println!("next_state is {next_state:?}");
+                let next_state = read_handshake_next_state(&packet).await?;
+                println!("next_state is {:?}", &next_state);
+                conn.state = next_state;
+
+                // TODO: CLEANUP THIS MESS. Done hastily to check if it would work (it works!!).
+
+                if let ConnectionState::Status = conn.state {
+                    // Send JSON
+                    let json = r#"{"version":{"name":"1.21.2","protocol":768},"players":{"max":100,"online":5,"sample":[{"name":"thinkofdeath","id":"4566e69f-c907-48ee-8d71-d7ba5aa00d20"}]},"description":{"text":"Hello, CactusMC!"},"favicon":"data:image/png;base64,<data>","enforcesSecureChat":false}"#;
+
+                    // TODO: Make a packet builder!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                    let lsp_packet_json = string::write(json)?;
+                    let lsp_packet_id: u8 = 0x00;
+                    let lsp_packet_len =
+                        varint::write((lsp_packet_json.len() + size_of::<u8>()) as i32);
+
+                    let mut lsp_packet: Vec<u8> = Vec::new();
+                    lsp_packet.extend_from_slice(&lsp_packet_len);
+                    lsp_packet.push(lsp_packet_id);
+                    lsp_packet.extend_from_slice(&lsp_packet_json);
+
+                    if let Err(e) = conn.socket.write_all(&lsp_packet).await {
+                        error!("Failed to write JSON to client: {e}");
+                    }
+                }
             }
             _ => {
                 warn!("packet id is 0x00 but State is not yet supported");
@@ -126,8 +154,40 @@ async fn handle_packet(
 
 async fn read_handshake_next_state(packet: &Packet<'_>) -> Result<ConnectionState, CodecError> {
     let data = packet.get_payload();
+    let mut offset: usize = 0;
 
-    let bytes_protocol_version = varint::read(data);
+    let protocol_version: (i32, usize) = varint::read(data)?;
+    offset += protocol_version.1;
+    info!("Handshake protocol version received: {protocol_version:?}");
 
-    Ok(ConnectionState::Handshake)
+    let server_address: (String, usize) = string::read(&data[offset..])?;
+    offset += server_address.1;
+    info!("Handshake server address received: {server_address:?}");
+
+    // Read 2 bytes
+    let mut slice = &data[offset..offset + 2]; // Create a slice of the two bytes
+    let server_port: u16 = byteorder::ReadBytesExt::read_u16::<byteorder::BigEndian>(&mut slice)
+        .expect("Unable to read port");
+    info!("Handshake server port received: {server_port}");
+    offset += 2;
+
+    let next_state: (i32, usize) = varint::read(&data[offset..])?;
+    info!("Handshake next state received: {next_state:?}");
+
+    match next_state.1 {
+        1 => {
+            // 1 is for status
+            debug!("Next state from handshake is status (1)");
+            Ok(ConnectionState::Status)
+        }
+        2 => {
+            // 2 is for login
+            error!("Next state from handshake login (2) not yet supported!");
+            gracefully_exit(0);
+        }
+        _ => {
+            error!("Next state from handshake not yet supported!");
+            gracefully_exit(0);
+        }
+    }
 }
