dcfe1477f6be8b166b51e3da625357d42553414b
Urpagin <urpagin@proton.me>
2025-08-26 21:26:41 +0200

    Auto-reformat project

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
 > [!IMPORTANT]
-> This repository is a **work-in-progress** (non-functional) and an experimental project by beginners. It's under active development; expect changes and incomplete features.
+> This repository is a **work-in-progress** (non-functional) and an experimental project by beginners. It's under active
+> development; expect changes and incomplete features.
 
 # 🎮 Copper: Minecraft Server in Rust
 
 ## Introduction
 
-Copper is a project aimed at creating a Minecraft server using the Rust programming language. The goal is to explore Rust's potential in game server development and demonstrate how to implement a complex game protocol in a performant and secure way. Rust is well-known for its memory safety features without compromising performance, making it an excellent choice for this type of project.
-
+Copper is a project aimed at creating a Minecraft server using the Rust programming language. The goal is to explore
+Rust's potential in game server development and demonstrate how to implement a complex game protocol in a performant and
+secure way. Rust is well-known for its memory safety features without compromising performance, making it an excellent
+choice for this type of project.
 
 ## Technologies Used
 
 - **Programming Language:** Rust
-
-
 
 ## License
 
@@ -25,4 +26,5 @@ Copper is licensed under the Mozilla Public License Version 2.0. See the `LICENS
 
 ---
 
-For any questions or comments, feel free to open an issue or contact me directly at the email: [minecraft.cactus@proton.me](mailto:minecraft.cactus@proton.me)
+For any questions or comments, feel free to open an issue or contact me directly at the
+email: [minecraft.cactus@proton.me](mailto:minecraft.cactus@proton.me)
--- src/net/packet/packet_types.rs
+++ src/net/packet/packet_types.rs
@@ -214,7 +214,7 @@ pub mod handshake {
             let protocol_version: VarInt = VarInt::consume_from_bytes(&mut data, ())?;
             let server_address: StringProtocol = StringProtocol::consume_from_bytes(&mut data, ())?;
             let server_port: UnsignedShort = UnsignedShort::consume_from_bytes(&mut data, ())?;
-            let next_state: NextState = NextState::new(VarInt::consume_from_bytes(&mut data,())?)?;
+            let next_state: NextState = NextState::new(VarInt::consume_from_bytes(&mut data, ())?)?;
 
             let packet: Packet = PacketBuilder::new()
                 .append_bytes(protocol_version.get_bytes())
@@ -465,16 +465,16 @@ pub mod configuration {
 
             // If the layout of the Array is not three StringProtocol.
             for pack in &known_packs {
-                for inner_type in &(*pack.get_value()){
+                for inner_type in &(*pack.get_value()) {
                     let cast_type: DataType = (*inner_type).clone().into();
                     if cast_type != DataType::StringProtocol {
                         return Err(
                             PacketError::Codec(
-                        CodecError::Encoding(DataType::Other("ClientboundKnownPacks"), ErrorReason::InvalidFormat(
-                            format!("The pack array data types must be three consecutive StringProtocol. Pack: {pack:?}")
-                        ))
+                                CodecError::Encoding(DataType::Other("ClientboundKnownPacks"), ErrorReason::InvalidFormat(
+                                    format!("The pack array data types must be three consecutive StringProtocol. Pack: {pack:?}")
+                                ))
                             )
-                    );
+                        );
                     }
                 }
             }
